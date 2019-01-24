092fae8c02116f93d1a76e4cb3b873d2ac67524a
jesslns <jesslns@yahoo.com.hk>
2019-01-24 14:13:19 +0000

    Developing coundown method

--- app.rb
+++ app.rb
@@ -14,9 +14,15 @@ end
 post '/countdown' do
   @day = params[:day]
   @month = params[:month]
-  @CD_date = Time.new(@month,@day)
   @now = Time.new;
-  #@countdown = (@CD_date - @now)/(3600*24)
+  @day_now = @now.day
+  @month_now = @now.month
+  #if @month < @month_now && @day < @day_now
+    #@year = @now.year+1
+  #else @year = @now.yeare
+#  end
+  #@countdown_date = Time.new(@year,@month,@day)
+  #@countdown = @countdown_date - @now
   erb(:countdown)
 end
 
